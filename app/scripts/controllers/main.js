a4e98de538d34899328aea79ba115db7490a6106
WonderBeat <borov.htid@gmail.com>
2013-09-08 17:51:05 +0400

    main screen styled

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -3,6 +3,7 @@
 angular.module('findMyCarApp').controller('MainCtrl', ['$scope', 'localStorageService', 'geolocation', function ($scope, storage, geolocation) {
 	$scope.carPosition = storage.get('car-position');
 	$scope.locationServicesAvailable = 'geolocation' in navigator;
+	$scope.deviceOrientationAvailable = 'DeviceOrientationEvent' in window;
 
 	var setLocationReceived = function (enabled) {
 		$scope.locationRecieved = enabled;
