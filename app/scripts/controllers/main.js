63466d0aff136b168b9f225c2b0923aaadaff0f8
OctaDecimal <borov.htid@gmail.com>
2013-09-13 12:52:50 +0400

    Copyright

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,3 +1,17 @@
+/*
+ * Copyright (c) 2013 Denis Golovachev
+ *
+ * This file is part of application, that is free software:
+ * you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License.
+ *
+ * Application is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with application.  If not, see <http ://www.gnu.org/licenses/>.
+ */
+
 'use strict';
 
 angular.module('findMyCarApp').controller('MainCtrl', ['$scope', 'localStorageService', 'geolocation', function ($scope, storage, geolocation) {
